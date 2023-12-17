60db20dc25dbf16e6074161fb7d6e269337ddc15
“hieppx” <hieppx@vnext.vn>
2023-12-17 13:52:12 +0700

    update home

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:vehicle_identification/generated/l10n.dart';
 import 'package:vehicle_identification/screen/home/provider/home_provider.dart';
@@ -25,12 +26,59 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
+  Future<void> _showMyDialog(Size size, HomeProvider p) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Text(
+              S.of(context).allVehicle.toUpperCase(),
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          content: SizedBox(
+            width: double.maxFinite,
+            // height: size.height * 0.3,
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                shrinkWrap: true,
+                itemCount: p.vehicles.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    style: ListTileStyle.drawer,
+                    leading: p.vehicles[index].type == 0
+                        ? const Icon(
+                            FontAwesomeIcons.motorcycle,
+                            color: AppColor.primary,
+                          )
+                        : const FaIcon(
+                            FontAwesomeIcons.car,
+                            color: AppColor.primary,
+                          ),
+                    onTap: () {
+                      p.setIndexVehicle(index);
+                      Navigator.pop(context);
+                    },
+                    title: Text(p.vehicles[index].model ?? ''),
+                    subtitle: Text(p.vehicles[index].vehicleID ?? ''),
+                  );
+                }),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return ChangeNotifierProvider(
         create: (_) => _provider,
         builder: (context, widgets) =>
             Consumer<HomeProvider>(builder: (context, value, child) {
+              bool isNotEmpty = value.vehicles.isNotEmpty;
               return Scaffold(
                 backgroundColor: Colors.grey.shade100,
                 extendBodyBehindAppBar: true,
@@ -39,22 +87,35 @@ class _HomeScreenState extends State<HomeScreen> {
                   backgroundColor: Colors.transparent,
                   elevation: 0,
                 ),
-                floatingActionButton: FloatingActionButton(onPressed: () {}),
+                floatingActionButton: value.vehicles.length > 1
+                    ? FloatingActionButton(
+                        backgroundColor: AppColor.blue4,
+                        onPressed: () {
+                          _showMyDialog(size, value);
+                        },
+                        child: const Icon(
+                          Icons.car_repair,
+                          color: AppColor.white,
+                        ),
+                      )
+                    : Container(),
                 body: Column(
                   children: [
                     ProfileHeader(
                       user: user!,
-                      vehicleID: value.vehicleInfor!.vehicleID ?? '',
+                      vehicleID: isNotEmpty
+                          ? value.vehicles[value.indexVehicle].vehicleID ?? ''
+                          : '',
                       isCheckExpires: value.isCheckExpires,
                     ),
                     const SizedBox(
                       height: 20,
                     ),
-                    value.vehicles.isNotEmpty
+                    isNotEmpty
                         ? Expanded(
                             child: SingleChildScrollView(
                               child: VehicleInformation(
-                                  vehicle: value.vehicleInfor!,
+                                  vehicle: value.vehicles[value.indexVehicle],
                                   isCheckExpires: value.isCheckExpires),
                             ),
                           )
--- lib/screen/home/provider/home_provider.dart
+++ lib/screen/home/provider/home_provider.dart
@@ -3,9 +3,6 @@ import 'package:vehicle_identification/model/vehicle.dart';
 import 'package:vehicle_identification/service/user/vehicle_service.dart';
 
 class HomeProvider extends ChangeNotifier {
-  Vehicle _vehicleInfor = Vehicle();
-  Vehicle? get vehicleInfor => _vehicleInfor;
-
   final VehicleService _vehicleService = VehicleService();
 
   List<Vehicle> _vehicles = [];
@@ -14,18 +11,25 @@ class HomeProvider extends ChangeNotifier {
   bool _isCheckExpires = false;
   bool get isCheckExpires => _isCheckExpires;
 
+  int _indexVehicle = 0;
+  int get indexVehicle => _indexVehicle;
+
   getVehicleInfor(String email) async {
     _vehicles = await _vehicleService.getVehicle(email);
     if (_vehicles.isNotEmpty) {
-      _vehicleInfor = _vehicles[0];
       checkExpires();
     }
     notifyListeners();
   }
 
   checkExpires() {
-    int dateExpires = int.parse(_vehicleInfor.expires!.replaceAll('-', ''));
-    if (getDate() < dateExpires) _isCheckExpires = true;
+    int dateExpires =
+        int.parse(_vehicles[_indexVehicle].expires!.replaceAll('-', ''));
+    if (getDate() < dateExpires) {
+      _isCheckExpires = true;
+    } else {
+      _isCheckExpires = false;
+    }
     notifyListeners();
   }
 
@@ -37,5 +41,11 @@ class HomeProvider extends ChangeNotifier {
         dateNow.month >= 10 ? dateNow.month.toString() : '0${dateNow.month}';
     convertDateNow = int.parse('${dateNow.year}$month$day');
     return convertDateNow;
+  }
+
+  setIndexVehicle(int i) {
+    _indexVehicle = i;
+    checkExpires();
+    notifyListeners();
   }
 }
